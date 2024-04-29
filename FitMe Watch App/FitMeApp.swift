c55a48ccf681e4ad756baccd871a71028ec589bd
Agfi <thoriq.aghfi60@gmail.com>
2024-04-29 16:26:50 +0700

    Initial Commit

---
+++ FitMe Watch App/FitMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitMeApp.swift
+//  FitMe Watch App
+//
+//  Created by Agfi on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FitMe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
